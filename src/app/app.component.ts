1a813a51cb9bab5fa5ab03d18a981061b07da5fc
Hans Schedel <hans.haugusth@gmail.com>
2023-11-03 07:13:21 -0300

    feat: add app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { EquipmentsService } from './services/equipment/equipments.service';
+import { Observable } from 'rxjs';
+import { PointsService } from './services/points/points.service';
+import { Equipment, EquipmentResponse } from './models/equipment/equipment.model';
+import { faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { PointResponse, PointUpdate } from './models/point/point.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,143 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'siemens-test-front';
+  title = 'Siemens Test API Front';
+  faPencil = faPencil
+  faTrash = faTrash
+  faPlus = faPlus
+
+  equipments$ = new Observable<EquipmentResponse[]>();
+  points$ = new Observable<PointResponse[]>();
+
+  // equipment form
+  equipmentId = ''
+  equipment: Equipment = {
+    name: '',
+    serialNumber: ''
+  }
+
+  // point form
+  pointId = ''
+  pointValue = ''
+  point: PointUpdate = {
+    name: '',
+    dataType: '',
+    equipmentId: '',
+  }
+
+  constructor(
+    private equipmentsService: EquipmentsService,
+    private pointService: PointsService) {
+    this.getEquipments()
+    this.getPoints()
+  }
+
+  addPointFromEquipment(id: string) {
+    this.point.equipmentId = id
+  }
+
+  getEquipments() {
+    this.equipments$ = this.equipmentsService.getAllEquipments()
+  }
+
+  getPoints() {
+    this.points$ = this.pointService.getAllPoints()
+  }
+
+  onSaveEquipament() {
+    if (this.equipment.name === '' || this.equipment.serialNumber === '') {
+      return
+    }
+
+    if (this.equipmentId !== '') {
+      return this.equipmentsService
+      .updateEquipment(this.equipment, this.equipmentId)
+      .subscribe(_ => {
+        this.getEquipments()
+        this.equipment = {
+          name: '',
+          serialNumber: ''
+        }
+        this.equipmentId = ''
+      })
+    }
+
+    return this.equipmentsService
+      .createEquipment(this.equipment)
+      .subscribe(_ => {
+        this.getEquipments()
+        this.equipment = {
+          name: '',
+          serialNumber: ''
+        }
+        this.equipmentId = ''
+      })
+  }
+
+  onSavePoint() {
+    if (this.point.name === ''
+        || this.point.dataType === ''
+        || this.point.equipmentId === ''
+        || this.pointValue === '') {
+      return
+    }
+
+    if (this.pointId !== '') {
+      return this.pointService
+      .updatePoint({...this.point, ...(this.pointValue !== '' && {value: Number(this.pointValue)})}, this.pointId)
+      .subscribe(_ => {
+        this.getPoints()
+        this.point = {
+          name: '',
+          dataType: '',
+          equipmentId: ''
+        }
+        this.pointId = ''
+        this.pointValue = ''
+      })
+    }
+
+    return this.pointService
+      .createPoint({
+        name: this.point.name!,
+        dataType: this.point.dataType!,
+        equipmentId: this.point.equipmentId!,
+        value: Number(this.pointValue)
+      })
+      .subscribe(_ => {
+        this.getPoints()
+        this.point = {
+          name: '',
+          dataType: '',
+          equipmentId: ''
+        }
+        this.pointId = ''
+        this.pointValue = ''
+      })
+  }
+
+  editFields(equipment: EquipmentResponse) {
+    this.getEquipments()
+    this.equipment = {...equipment}
+    this.equipmentId = equipment._id
+  }
+
+  editPointFields(point: PointResponse) {
+    this.getPoints()
+    this.point = {...point}
+    this.pointId = point._id
+    this.pointValue = point.value.toString()
+  }
+
+  deleteEquipament(id: string) {
+    this.equipmentsService
+      .deleteEquipament(id)
+      .subscribe(_ => this.getEquipments())
+  }
+
+  deletePoint(id: string) {
+    this.pointService
+      .deletePoint(id)
+      .subscribe(_ => this.getPoints())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

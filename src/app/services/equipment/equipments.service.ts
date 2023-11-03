16631608ce17b5ebabe961bb34e088e93a1629cb
Hans Schedel <hans.haugusth@gmail.com>
2023-11-03 05:50:10 -0300

    reafctor: move equipment model

--- src/app/services/equipment/equipments.service.ts
+++ src/app/services/equipment/equipments.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { environment } from 'src/environments/environment'
-import { Equipment, EquipmentResponse, EquipmentUpdate } from 'src/app/models/equipment.model'
+import { Equipment, EquipmentResponse, EquipmentUpdate } from 'src/app/models/equipment/equipment.model'
 
 @Injectable({
   providedIn: 'root',
